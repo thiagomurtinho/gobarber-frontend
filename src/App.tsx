cb965d9d0b09d98beec4b938addd73de7e56c906
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-24 23:14:06 -0300

    Start application for frontend
    Separate folders for create integration

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,112 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { FiLogIn, FiMail } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErros';
+
+import logoImg from '../../assets/logo.svg';
+
+import { AnimationContainer, Background, Container, Content } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { useToast } from '../../hooks/toast';
+
+import api from '../../services/api';
+
+interface ForgotPasswordFormData {
+  email: string;
+  password: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      try {
+        setLoading(true);
+
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um e-mail válido')
+            .required('E-mail obrigatório'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        // Password recovery
+        await api.post('/password/forgot', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'sucess',
+          title: 'E-mail de recuperação enviado',
+          description:
+            'Enviamos um e-mail para verificar a recuperação de senha, cheque sua caixa de entrada.',
+        });
+
+        // history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na recuperação de senha',
+          description: 'Erro ao tentar recuperar senha, cheque as credenciais',
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber Logo" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Recuperar senha</h1>
+
+            <Input
+              name="email"
+              icon={FiMail}
+              type="text"
+              placeholder="E-mail"
+            />
+
+            <Button loading={loading} type="submit">
+              Recuperar
+            </Button>
+          </Form>
+
+          <Link to="/signin">
+            <FiLogIn />
+            Voltar ao login
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
